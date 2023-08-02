0cbd57dfba0988ed4de8d6f70029e3de81b95533
Nik0lasto <nikolas2004rodriguez@gmail.com>
2023-08-02 08:55:06 -0500

    Fin Excel

---
+++ ProyectoExcel/Controllers/HomeController.cs
@@ -0,0 +1,117 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProyectoExcel.Models;
+using System.Diagnostics;
+
+using NPOI.SS.UserModel;    //Usar Modelo
+using NPOI.XSSF.UserModel;
+using NPOI.HSSF.UserModel;  //Leer solo un archivo (xlsx)
+using ProyectoExcel.Models.ViewModels;
+using EFCore.BulkExtensions;
+
+namespace ProyectoExcel.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly DbExcelContext _dbcontext;
+
+        public HomeController(DbExcelContext context)
+        {
+            _dbcontext = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult MostrarDatos([FromForm] IFormFile ArchivoExcel)
+        {
+            Stream stream = ArchivoExcel.OpenReadStream();   //Leer el archivo excel en memoria
+
+            IWorkbook MiExcel = null;
+
+            if(Path.GetExtension(ArchivoExcel.FileName) == ".xlsx")
+            {
+                MiExcel = new XSSFWorkbook(stream); //El archivo sea leidoo con este recurso
+            }
+            else
+            {
+                MiExcel = new XSSFWorkbook(stream);
+            }
+
+            ISheet HojaExcel = MiExcel.GetSheetAt(0); //Obtener solo la primera Hoja
+
+            int cantidadFilas = HojaExcel.LastRowNum;    //Cantidad de filas de la hoja excel
+
+            List<VMContacto> Lista = new List<VMContacto>();
+
+            for(int i = 1; i <= cantidadFilas; i++)
+            {
+                IRow fila = HojaExcel.GetRow(i);    //Devolver una hoja por cada iteracion
+
+                Lista.Add(new VMContacto
+                {
+                    nombre = fila.GetCell(0).ToString(),
+                    apellido = fila.GetCell(1).ToString(),
+                    telefono = fila.GetCell(2).ToString(),
+                    correo = fila.GetCell(3).ToString()
+                });
+            }
+
+            return StatusCode(StatusCodes.Status200OK, Lista); //Devuelve en forma de lista
+        }
+
+        //Envio de Datos
+        [HttpPost]
+        public IActionResult EnviarDatos([FromForm] IFormFile ArchivoExcel)
+        {
+            Stream stream = ArchivoExcel.OpenReadStream();   //Leer el archivo excel en memoria
+
+            IWorkbook MiExcel = null;
+
+            if (Path.GetExtension(ArchivoExcel.FileName) == ".xlsx")
+            {
+                MiExcel = new XSSFWorkbook(stream); //El archivo sea leidoo con este recurso
+            }
+            else
+            {
+                MiExcel = new XSSFWorkbook(stream);
+            }
+
+            ISheet HojaExcel = MiExcel.GetSheetAt(0); //Obtener solo la primera Hoja
+
+            int cantidadFilas = HojaExcel.LastRowNum;    //Cantidad de filas de la hoja excel
+
+            List<Contacto> Lista = new List<Contacto>();
+
+            for (int i = 1; i <= cantidadFilas; i++)
+            {
+                IRow fila = HojaExcel.GetRow(i);    //Devolver una hoja por cada iteracion
+
+                Lista.Add(new Contacto
+                {
+                    Nombre = fila.GetCell(0).ToString(),
+                    Apellido = fila.GetCell(1).ToString(),
+                    Telefono = fila.GetCell(2).ToString(),
+                    Correo = fila.GetCell(3).ToString()
+                });
+            }
+
+            _dbcontext.BulkInsert(Lista);
+
+            return StatusCode(StatusCodes.Status200OK, new { mensaje = "ok"});  //Devuelve en forma de lista
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ProyectoExcel/Models/DbExcelContext.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace ProyectoExcel.Models;
+
+public partial class DbExcelContext : DbContext
+{
+    public DbExcelContext()
+    {
+    }
+
+    public DbExcelContext(DbContextOptions<DbExcelContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Contacto> Contactos { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=PC-NIK0\\SQLEXPRESS; DataBase = db_Excel;Integrated Security=true; TrustServerCertificate=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Contacto>(entity =>
+        {
+            entity.HasKey(e => e.IdContacto).HasName("PK__Contacto__A4D6BBFAAA7D7953");
+
+            entity.ToTable("Contacto");
+
+            entity.Property(e => e.Apellido)
+                .HasMaxLength(40)
+                .IsUnicode(false);
+            entity.Property(e => e.Correo)
+                .HasMaxLength(40)
+                .IsUnicode(false);
+            entity.Property(e => e.Nombre)
+                .HasMaxLength(40)
+                .IsUnicode(false);
+            entity.Property(e => e.Telefono)
+                .HasMaxLength(40)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
